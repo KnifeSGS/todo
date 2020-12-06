c18ea50a32661e51d1d9c819f62c0c4050a0f6c0
KnifeSGS <knife.sgs@gmail.com>
2020-12-06 23:50:03 +0100

    hiányzik: hide function, clear, animations

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const todoForm = document.querySelector('.todo__form');
 const todoInput = document.querySelector('.todo__input');
 const todoItemsList = document.querySelector('.todo__items');
 let todos = [];
+let uncheckedTodos = 0;
+let checkedCounter = 0;
 
 
 todoForm.addEventListener('submit', function (event) {
@@ -49,14 +51,47 @@ function renderTodos(todos) {
     const liTemplate = `
       <input type="checkbox" class="todo__checkbox" ${checked}>
       <span class="item__text">${item.name}</span>
-      <button class="todo__deleteBtn"><i class="fa fa-trash" aria-hidden="true"></i></button>
+      <button type="button" class="todo__deleteBtn"><i class="fa fa-trash" aria-hidden="true"></i></button>
       `;
     li.insertAdjacentHTML('afterbegin', liTemplate);
 
     todoItemsList.append(li);
   });
-  
-  document.querySelector('.item__count').textContent = todos.length;
+  uncheckedCounter();
+  dayToday();
+  dateToday();
+}
+
+const dayToday = () => {
+  const now = new Date();
+  const weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+  const today = weekday[now.getDay()];
+  // return today;
+  document.querySelector('.date__dayName').textContent = today;
+}
+
+const dateToday = () => {
+  const now = new Date();
+  const date = now.toLocaleDateString('nl-NL')
+  // return date;
+  document.querySelector('.date__today').textContent = date;
+}
+
+const uncheckedCounter = () => {
+  let counter = [];
+  todos.filter(item => {
+    if (item.completed === false){
+      counter.push(item);
+    }
+  })
+  document.querySelector('.item__count').textContent = counter.length;
 }
 
 function addToLocalStorage(todos) {
@@ -69,8 +104,9 @@ function getFromLocalStorage() {
   if (reference) {
     todos = JSON.parse(reference);
     renderTodos(todos);
-  }
+    }
 }
+
 
 function toggle(id) {
   todos.forEach(function (item) {
